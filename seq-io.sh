2bb4349c8e148c76367418498e649e8392fcfe3e
Martyn Gigg <martyn.gigg@gmail.com>
2024-01-04 14:07:47 +0000

    Add comment regarding dd speed column

--- seq-io.sh
+++ seq-io.sh
@@ -4,6 +4,9 @@
 niterations=5
 iflags=nocache,fullblock
 oflags=nocache
+# In some unspecified dd version the output format changed and the speed column changed
+# This test is not ideal but it was primarily running between centos 7 and rocky 8 so was
+# good enough...
 speed_column=4
 if dd --version | head -n1 | grep 8.2 >/dev/null; then
   speed_column=3
